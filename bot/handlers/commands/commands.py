31f20cdc74ec211fc3297fa33decceeb71023504
Dmitry <diprog991@gmail.com>
2023-04-30 17:28:33 +0300

    Команда /commands

---
+++ bot/handlers/commands/commands.py
@@ -0,0 +1,10 @@
+from aiogram.filters import Command
+from aiogram.types import Message
+
+from bot.handlers.methods import update_commands
+from bot.router import router
+
+
+@router.message(Command('commands'))
+async def command_commands_handler(message: Message) -> None:
+    await update_commands(message.from_user.id)
