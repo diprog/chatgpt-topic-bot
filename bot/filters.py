be331bc8c7fff7c0c5d2b15902273dcdffcf1c55
Dmitry <diprog991@gmail.com>
2023-04-30 16:36:23 +0300

    Перенести фильтры из methods.py в filters.py

---
+++ bot/filters.py
@@ -0,0 +1,52 @@
+from aiogram import types
+
+import db.settings
+
+
+async def chat_is_forum(message: types.Message) -> bool:
+    if not message.chat.is_forum:
+        await message.answer('❗️ Команда доступна только в группе с топиками.')
+        return False
+    return True
+
+
+async def user_is_bot_admin(message: types.Message) -> bool:
+    settings = await db.settings.get()
+    if not settings.is_bot_admin(message.from_user.id):
+        await message.reply('❗️ Команда доступна только администрации бота.')
+        return False
+    return True
+
+
+async def user_is_group_admin(message: types.Message) -> bool:
+    chat_member = await message.chat.get_member(message.from_user.id)
+    if not isinstance(chat_member, (types.ChatMemberOwner, types.ChatMemberAdministrator)):
+        if message.sender_chat and message.sender_chat.id != message.chat.id:
+            await message.reply('❗️ Команда доступна только владельцу группы.')
+            return False
+        await message.reply('❗️ Команда доступна только владельцу группы.')
+        return False
+    return True
+
+
+async def message_is_in_topic(message: types.Message, topic=True):
+    if not message.message_thread_id and topic:
+        await message.reply('❗️ Команда может быть использована только внутри темы.')
+        return False
+    return True
+
+
+async def topic_filter(message: types.Message, topic=True):
+    if not await chat_is_forum(message):
+        return False
+
+    if not await user_is_bot_admin(message):
+        return False
+
+    if not await user_is_group_admin(message):
+        return False
+
+    if not await message_is_in_topic(message, topic):
+        return False
+
+    return True
--- bot/handlers/methods.py
+++ bot/handlers/methods.py
@@ -49,26 +49,3 @@ async def send_logging_message(user: types.User, text: str) -> types.Message:
                 print('kicked')
 
 
-async def topic_filter(message: types.Message, topic=True):
-    if not message.chat.is_forum:
-        await message.answer('❗️ Команда доступна только в группе с топиками.')
-        return False
-
-    settings = await db.settings.get()
-    if not settings.is_bot_admin(message.from_user.id):
-        await message.reply('❗️ Команда доступна только администрации бота.')
-        return False
-
-    chat_member = await message.chat.get_member(message.from_user.id)
-    if not isinstance(chat_member, (types.ChatMemberOwner, types.ChatMemberAdministrator)):
-        if message.sender_chat and message.sender_chat.id != message.chat.id:
-            await message.reply('❗️ Команда доступна только владельцу группы.')
-            return False
-        await message.reply('❗️ Команда доступна только владельцу группы.')
-        return False
-
-    if not message.message_thread_id and topic:
-        await message.reply('❗️ Команда может быть использована только внутри топика.')
-        return False
-
-    return True
