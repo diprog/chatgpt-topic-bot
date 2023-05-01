e07dd65d5b38c23f94736e872d6228ab053fc2f1
Dmitry <diprog991@gmail.com>
2023-05-01 11:45:16 +0300

    Фикс

--- bot/handlers/commands/set.py
+++ bot/handlers/commands/set.py
@@ -9,7 +9,7 @@ from bot import router
 @router.message(Command('set'))
 async def command_start_handler(message: Message) -> None:
     if await topic_filter(message):
-        group_settings = await db.get(message.chat.id)
+        group_settings = await db.group_settings.get(message.chat.id)
         group_settings.active_topic_id = message.message_thread_id
         await group_settings.save()
         await message.reply('✅ Теперь бот будет работать внутри этого топика.')
